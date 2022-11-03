993177662d0cec32159ecc2da413eaa5056ce1d9
MaryRabinovich <mary.rabinovich@mail.ru>
2022-11-03 10:06:10 +0300

    bugfix: - mobile styling - input num-cols validation

--- src/controllers/input.js
+++ src/controllers/input.js
@@ -3,10 +3,10 @@ const numCols = document.getElementById('num-cols')
 const min = numCols.getAttribute('min')
 const max = numCols.getAttribute('max')
 
-globalThis.state.set(numCols.id, numCols.value)
+globalThis.state.set(numCols.id, parseInt(numCols.value))
 
 numCols.addEventListener('change', () => {
-    if (numCols.value < min) numCols.value = min
-    if (numCols.value > max) numCols.value = max
-    globalThis.state.set(numCols.id, numCols.value)
+    if (parseInt(numCols.value) < min) numCols.value = min
+    if (parseInt(numCols.value) > max) numCols.value = max
+    globalThis.state.set(numCols.id, parseInt(numCols.value))
 })
